99ede4f427c9b6a3a23b1b5f0e7177b9dc487986
Anderson Freitas <freitas.dev@proton.me>
2024-11-12 20:08:46 -0300

    :green_heart: Teste CI
    Teste CI

--- freitascodes/settings/env.py
+++ freitascodes/settings/env.py
@@ -1,10 +1,11 @@
 from freitascodes.settings.base import *
 from dotenv import load_dotenv
+import os
 
 DEBUG = True
 
-if DEBUG:
-  load_dotenv()
+# if DEBUG:
+#   load_dotenv()
 
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
